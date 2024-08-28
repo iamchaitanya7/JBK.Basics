5292856fc98e525607cfaf9ee84a18d6ed81fe79
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-08-29 01:01:34 +0530

    Initial Polymorphism

---
+++ src/com/jbk/oops/polymorphism/assignments/BaseClass.java
@@ -0,0 +1,8 @@
+package com.jbk.oops.polymorphism.assignments;
+
+public class BaseClass {
+    public void method () { // Base class method
+        System.out.println ("I'm the method of BaseClass");
+    }
+}
+
---
+++ src/com/jbk/oops/polymorphism/assignments/DerivedClass.java
@@ -0,0 +1,7 @@
+package com.jbk.oops.polymorphism.assignments;
+
+public class DerivedClass extends BaseClass {
+    public void method () {// Base class method
+        System.out.println ("I'm the method of DerivedClass");
+    }
+}
