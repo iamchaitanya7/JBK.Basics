28423144546f7819d85c58a9f161e2fc3d230231
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-08-29 17:12:25 +0530

    Initial Collections

--- src/com/jbk/collections/Test_Product.java
+++ src/com/jbk/collections/Test_Product.java
@@ -3,7 +3,12 @@ package com.jbk.collections;
 import java.util.ArrayList;
 
 public class Test_Product {
+    void lineSeparator () {
+        System.out.println ("====================================================");
+    }
+
     public static void main (String[] args) {
+        Test_Product obj = new Test_Product ( );
         Product p1 = new Product (101, "Notebook", 500);
         Product p2 = new Product (102, "College Bag", 2500);
         Product p3 = new Product (103, "Pen", 1000);
@@ -11,7 +16,16 @@ public class Test_Product {
         Product p5 = new Product (105, "Laptop", 50000);
 
         ArrayList<Product> productArrayList = new ArrayList<> ( );
+        productArrayList.add (p1);
+        productArrayList.add (p2);
+        productArrayList.add (p3);
+        productArrayList.add (p4);
+        productArrayList.add (p5);
 
-
+        obj.lineSeparator ( );
+        for (Product p : productArrayList) {
+            System.out.println (p);
+        }
+        obj.lineSeparator ( );
     }
 }
