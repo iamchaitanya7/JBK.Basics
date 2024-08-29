3956e6cbf024fae73114824758baddeae82196b1
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-08-29 16:48:16 +0530

    Initial Collections

---
+++ src/com/jbk/collections/Test_Product.java
@@ -0,0 +1,11 @@
+package com.jbk.collections;
+
+public class Test_Product {
+    public static void main (String[] args) {
+        Product p1 = new Product (101, "Notebook", 500);
+        Product p2 = new Product (102, "College Bag", 2500);
+        Product p3 = new Product (103, "Pen", 1000);
+        Product p4 = new Product (104, "Pendrive", 1500);
+        Product p5 = new Product (105, "Laptop", 50000);
+    }
+}
