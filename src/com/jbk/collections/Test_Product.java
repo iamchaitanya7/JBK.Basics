be781d32a5fd935d1eca56de931c0e886a04cccb
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-08-29 16:49:40 +0530

    Initial Collections

--- src/com/jbk/collections/Test_Product.java
+++ src/com/jbk/collections/Test_Product.java
@@ -1,5 +1,7 @@
 package com.jbk.collections;
 
+import java.util.ArrayList;
+
 public class Test_Product {
     public static void main (String[] args) {
         Product p1 = new Product (101, "Notebook", 500);
@@ -7,5 +9,9 @@ public class Test_Product {
         Product p3 = new Product (103, "Pen", 1000);
         Product p4 = new Product (104, "Pendrive", 1500);
         Product p5 = new Product (105, "Laptop", 50000);
+
+        ArrayList<Product> productArrayList = new ArrayList<> ( );
+
+
     }
 }
