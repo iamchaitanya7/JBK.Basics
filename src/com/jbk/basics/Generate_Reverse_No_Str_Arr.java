df98e3a965365f68a8ac4a47a9ebca01f485bd59
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-08-30 01:08:41 +0530

    Update Reverse_No_Str_Arr

--- src/com/jbk/basics/Generate_Reverse_No_Str_Arr.java
+++ src/com/jbk/basics/Generate_Reverse_No_Str_Arr.java
@@ -29,16 +29,25 @@ public class Generate_Reverse_No_Str_Arr {
     }
 
     //Method to Reverse the Given String using StringBuilder.
-    /*public String stringReversal(String input) {
+    /*
+    public String stringReversal(String input) {
         StringBuilder reversed = new StringBuilder(input);
         return reversed.reverse().toString();
-    }*/
+    }
+    */
 
     //Method to Reverse the Given Array.
     public void reverseArray (int[] arr) {
-        //System.out.println ("Array Reversal");
+        //Created a New Array that is initialized to the size of the coming {Array.length()}.
+        int[] resArr = new int[arr.length];
+        int j = 0;
         for (int i = arr.length - 1; i >= 0; i--) {
-            System.out.print (arr[i] + " ");
+            //System.out.println (arr[i] + " ");            //Can Also be used to display each reversed array element individually.
+            resArr[j] = arr[i];                            //Creation of New Array that stores the Reversed Array.
+            j++;
         }
+        System.out.println ("Reversed Array: " + Arrays.toString (resArr));
+        //Used { Arrays.to.String() } method to display the array elements directly.
+
     }
 }
