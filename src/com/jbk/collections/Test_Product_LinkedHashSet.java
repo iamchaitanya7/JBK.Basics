727ca2a04d0b3ff2fe60e2616a518b0dd3e1ef67
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-08-30 23:22:22 +0530

    Update Test_Product_LinkedHashSet

--- src/com/jbk/collections/Test_Product_LinkedHashSet.java
+++ src/com/jbk/collections/Test_Product_LinkedHashSet.java
@@ -17,47 +17,50 @@ public class Test_Product_LinkedHashSet {
         Product p4 = new Product (104, "Pendrive", 1500);
         Product p5 = new Product (105, "Laptop", 50000);
 
-        //linked List Creation from Collections Framework.
+        //LinkedHashSet Creation from Collections Framework.
         LinkedHashSet<Product> productLinkedHashSet = new LinkedHashSet<> ( );
-        //linkedList.add() Used to Remove the Specified Object from the ArrayList
+
+        //linkedHashSet.add() Used to Add the Specified Object to the LinkedHashSet.
         productLinkedHashSet.add (p1);
         productLinkedHashSet.add (p2);
         productLinkedHashSet.add (p3);
         productLinkedHashSet.add (p4);
         productLinkedHashSet.add (p5);
 
-        //linkedList.remove() Used to Remove the Specified Object from the ArrayList
+        //linkedHashSet.remove() Used to Remove the Specified Object from the LinkedHashSet.
         productLinkedHashSet.remove (p5);
 
-        //linkedList.clear() Used to Clear the Complete Objects from the ArrayList
+        //linkedHashSet.clear() Used to Clear the All Objects from the LinkedHashSet.
         //productLinkedList.clear ();
 
         productLinkedHashSet.add (p5);
         productLinkedHashSet.add (p5);
-        //We Can Add multiple Duplicate objects into a single Arraylist and Similarly we can also remove() this multiple Duplicate Objects.
+        //We Cannot Add multiple Duplicate objects into a Single LinkedHashSets.
 
+        //Using For Loop for Displaying the Data.
         obj.lineSeparator ( );
-        //Using For Loop to Display the Data.
         System.out.println ("Displaying Data Using For Loop");
         obj.lineSeparator ( );
         for (Product p : productLinkedHashSet) {
             System.out.println (p);
         }
-        obj.lineSeparator ( );
 
-        //Displays the size of LinkedList.
+        //linkedHashSet.size() Displays the size of LinkedList.
+        obj.lineSeparator ( );
         System.out.println ("Size of Arraylist: " + productLinkedHashSet.size ( ));
         obj.lineSeparator ( );
 
-        //Using ListIterator to Display the Data.
-        /*System.out.println ("Displaying Data Using ListIterator");
+        //Using ListIterator to Display the Data.      **Cannot Be Used in the Case of LinkedHashSet**
+        /*
+        System.out.println ("Displaying Data Using ListIterator");
         obj.lineSeparator ( );
         ListIterator<Product> listItr = productLinkedHashSet.listIterator ( );
         while (listItr.hasNext ( )) {
             Product t = listItr.next ( );
             System.out.println (t);
         }
-        obj.lineSeparator ( );*/
+        obj.lineSeparator ( );
+        */
 
         //Using Iterator to Display the Data.
         System.out.println ("Displaying Data Using Iterator");
@@ -70,14 +73,16 @@ public class Test_Product_LinkedHashSet {
         obj.lineSeparator ( );
 
         //Using the For Loop to Display Data.
-        /*System.out.println ("Displaying the Data using Simple For Loop");
+        /*
+        System.out.println ("Displaying the Data using Simple For Loop");
         obj.lineSeparator ( );
         for (int i = 0; i < productLinkedHashSet.size ( ); i++) {
-            System.out.println (productLinkedHashSet.get (i));
+            System.out.println (productLinkedHashSet.get (i));         //**Cannot Use the linkedHashSet.get() Method**
         }
-        obj.lineSeparator ( );*/
+        obj.lineSeparator ( );
+        */
 
-        //Using the For-Each Loop to Display Data Supported by Java-8.
+        //Using the For-Each Loop to Display Data. **Supported by Java-8.**
         System.out.println ("Displaying the Data using For-Each Loop");
         obj.lineSeparator ( );
         productLinkedHashSet.forEach (product -> System.out.println (product));
