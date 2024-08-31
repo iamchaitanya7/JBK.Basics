1c2a44b5f9efdab7d0029eb0c1fff42190a762a5
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-08-31 18:09:54 +0530

    Update Test_Product_HashSet

--- src/com/jbk/collections/Test_Product_HashSet.java
+++ src/com/jbk/collections/Test_Product_HashSet.java
@@ -2,7 +2,6 @@ package com.jbk.collections;
 
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.ListIterator;
 
 public class Test_Product_HashSet {
     void lineSeparator () {
@@ -10,7 +9,7 @@ public class Test_Product_HashSet {
     }
 
     public static void main (String[] args) {
-        Test_Product obj = new Test_Product ( );
+        Test_Product_HashSet obj = new Test_Product_HashSet ( );
         Product p1 = new Product (101, "Notebook", 500);
         Product p2 = new Product (102, "College Bag", 2500);
         Product p3 = new Product (103, "Pen", 1000);
