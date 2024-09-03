ef26b3e6be36448033bc3d2781718bbc4f9fb214
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-09-03 15:51:38 +0530

    Update testBank

--- src/com/jbk/projects/testbank/testBank.java
+++ src/com/jbk/projects/testbank/testBank.java
@@ -15,21 +15,29 @@ public class testBank {
             testBank test = new testBank ();
             //Assigning the Account Class Variables with Initial Details.
             test.lineSeparator ();
-            Account account = new Account (101, "Chaitanya", "9767", 1000.00);
-            System.out.println ("Initial " + account);
+            Account account1 = new Account (101, "Chaitanya", "9767", 1000.00);
+            //Account account2 = new Account (102, "Akash", "1818", 2000.00);
+            //Account account3 = new Account (103, "Pratik", "0103", 3000.00);
+            //Account account4 = new Account (104, "Sanket", "0104", 4000.00);
+            //Account account5 = new Account (105, "Chetan", "0105", 5000.00);
+
+            System.out.println ("Initial " + account1);
             test.lineSeparator ();
 
             //Creating Object of Deposit Class and Depositing Money into Account.
-            Deposit d = new Deposit (account, 101, "Chaitanya", 1500.00);
+            Deposit d = new Deposit (account1, "Chaitanya", "9767", 1500.00);
             test.lineSeparator ();
 
             //Creating Object of Withdraw Class and Withdrawing Money from Account for Specified Username.
-            Withdraw w = new Withdraw (account, 101, "Chaitanya", 500.00);
+            Withdraw w = new Withdraw (account1, "Chaitanya", "9767", 500);
             test.lineSeparator ();
 
             //Displaying the Final Updated Account Balance.
-            System.out.println("Final " + account);
+            System.out.println ("Final " + account1);
+
+            //Account.displayAllAccounts();
             test.lineSeparator ();
+
         }
         catch (Exception e) {
             e.printStackTrace();
