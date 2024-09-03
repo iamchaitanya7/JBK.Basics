2512655caa152dfe0a5c36d846d0deaeeced69e4
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-09-03 15:50:04 +0530

    Update Deposit

--- src/com/jbk/projects/deposit/Deposit.java
+++ src/com/jbk/projects/deposit/Deposit.java
@@ -1,21 +1,28 @@
 package com.jbk.projects.deposit;
 
 import com.jbk.projects.account.Account;
+import com.jbk.projects.account.*;
 
 public class Deposit {
 
-    public Deposit (Account acc, int accNo, String name, double amount) {
-        //Validating the Initial Account Details Whether Match or Not then Only Depositing the Money into Account.
-        if (accNo == acc.getAccNo () && name.equals (acc.getName ())) {
+    public Deposit (Account acc, String name, String pin, double amount) throws InvalidAmount, InvalidUsernamePin {
+        //Validating the Initial Account Details(Acc_No, Name) Whether Match or Not then Only Depositing the Money into Account.
+        if (pin == acc.getPin ( ) && name.equals (acc.getName ( ))) {
+
+            //Validating the Account Details(Name, Pin) Whether Match or Not then Only Depositing the Money into Account.
+            //acc = Account.findAccount(name, pin);
+            //if (acc != null) {
             if (amount > 0) {
                 Account.setBalance (Account.getBalance ( ) + amount);
                 System.out.println ("Deposited Amount: " + amount);
                 System.out.println ("Updated Account Balance: " + Account.getBalance ( ));
             } else {
-                System.out.println ("Deposit amount must be greater than zero.");
+                throw new InvalidAmount ("Invalid Amount! Enter Correct Amount");
+                //System.out.println ("Deposit amount must be greater than zero.");
             }
         } else {
-            System.out.println ("Account details do not match. Deposit failed.");
+            throw new InvalidUsernamePin ("Invalid Username and Pin! Account details do not match. Deposit failed.");
+            //System.out.println ("Invalid Username and Pin! Account details do not match. Deposit failed.");
         }
     }
 }
