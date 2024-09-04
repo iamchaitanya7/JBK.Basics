75c2dae2f1a99f2e999015495706f9dc9a58a9d4
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-09-04 15:06:40 +0530

    Update CollectionInterface_Ex

--- src/com/jbk/collections/CollectionInterface_Ex.java
+++ src/com/jbk/collections/CollectionInterface_Ex.java
@@ -1,4 +1,18 @@
 package com.jbk.collections;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class CollectionInterface_Ex {
+    public static void main (String[] args) {
+        Collection<Integer> integerCollection = new ArrayList<> ( );
+        integerCollection.add (10);
+        integerCollection.add (20);
+        integerCollection.add (30);
+        integerCollection.add (40);
+        integerCollection.add (50);
+
+        System.out.println (integerCollection);
+
+    }
 }
