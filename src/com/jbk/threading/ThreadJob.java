f3756d66a04f52fa46db31c106235f34ea61ad26
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-09-05 17:13:44 +0530

    Update ThreadJob

--- src/com/jbk/threading/ThreadJob.java
+++ src/com/jbk/threading/ThreadJob.java
@@ -1,12 +1,24 @@
 package com.jbk.threading;
 
+import java.util.*;
+
 public class ThreadJob extends Thread {
+
+    int count = 0;
 
     //Overridden the run() method from the Thread Class.
     @Override
     public void run () {
-        for (int i = 1; i < 10; i++) {
-            System.out.println (i);
+        for (int i = 1; i <= 50000; i++) {
+            //System.out.println (i);
+            increment ( );
         }
     }
+
+    synchronized void increment () {
+        //synchronized (this){ }     Can also we used for more specified detailed making of a specific element as synchronized rather than making all stuff as synchronized.
+        count += 1;
+
+    }
+
 }
