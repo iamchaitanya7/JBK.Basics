c9039693f05c2f90baa9d79c58b090c86396df51
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-09-05 18:11:52 +0530

    Update ThreadClientC

--- src/com/jbk/threading/ThreadClientC.java
+++ src/com/jbk/threading/ThreadClientC.java
@@ -1,10 +1,17 @@
 package com.jbk.threading;
 
+import javax.sound.midi.Soundbank;
+
 public class ThreadClientC {
-    public static void main (String[] args) {
+    public static void main (String[] args) throws InterruptedException {
         ThreadJobC jobC = new ThreadJobC ( );
 
         Thread t1 = new Thread (jobC);
+        System.out.println (t1.getState ( ));    //NEW State of the Thread
         t1.start ( );
+        System.out.println (t1.getState ( ));    //RUNNABLE State of the Thread.
+        t1.join ( );
+        System.out.println (t1.getState ( ));   //TERMINATED State of the Thread.
+
     }
 }
