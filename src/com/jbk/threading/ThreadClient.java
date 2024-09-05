637026e4a6df0858d01b94b31b99f85dde0a850a
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-09-05 16:39:52 +0530

    Update ThreadClient

--- src/com/jbk/threading/ThreadClient.java
+++ src/com/jbk/threading/ThreadClient.java
@@ -5,15 +5,28 @@ public class ThreadClient {
         System.out.println ("=========================================================");
     }
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws InterruptedException {
         ThreadClient line = new ThreadClient ( );
+
         System.out.println ("Thread Example....!");
         ThreadJob job = new ThreadJob ( );
-        //The below start() method is used to start the specified thread (class).
-        job.start ( );
-
-
         line.separator ( );
+
+        //The below start() method is used to start the specified thread (class).
+        //job.start ( );
+        Thread t1 = new Thread (job);
+        Thread t2 = new Thread (job);
+        Thread t3 = new Thread (job);
+
+        t1.start ( );
+        t2.start ( );
+        t3.start ( );
+
+        t1.join ( );
+        t2.join ( );
+        t3.join ( );
+
+        System.out.println ("Exit from Main....");
 
     }
 }
