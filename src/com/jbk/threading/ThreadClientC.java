98306f196a912a3752f5f1df658e4bc43c48c662
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-09-05 18:28:29 +0530

    Update ThreadClientC

--- src/com/jbk/threading/ThreadClientC.java
+++ src/com/jbk/threading/ThreadClientC.java
@@ -7,13 +7,21 @@ public class ThreadClientC {
         ThreadJobC jobC = new ThreadJobC ( );
 
         Thread t1 = new Thread (jobC);
-        System.out.println ("State of the Thread : " + t1.getState ( ));    //NEW State of the Thread
+        System.out.println ("ID of Thread: " + t1.getId ( ));                //Displays ID of the Specified Thread.
+        System.out.println ("Name of Thread: " + t1.getName ( ));            //Displays Name of the Specified Thread.
+        System.out.println ("Check Whether a Thread is Alive or Not: " + t1.isAlive ( ));
+
+        System.out.println ("State of the Thread : " + t1.getState ( ));    //NEW State of the Thread.
+
         t1.start ( );
+        System.out.println ("Check Whether a Thread is Alive or Not: " + t1.isAlive ( ));
         System.out.println ("State of the Thread : " + t1.getState ( ));    //RUNNABLE State of the Thread.
+
         t1.join ( );
         System.out.println ("State of the Thread : " + t1.getState ( ));   //TERMINATED State of the Thread.
+        System.out.println ("Current Active Count of Thread:  " + Thread.activeCount ( ));                             //Displays the Count of Current Active Threads.
 
         t1.setDaemon (true);         //Setting the NormalThread as Daemon Thread.
-
+        System.out.println ("Checking Whether a Thread is Daemon or Not: " + t1.isDaemon ( ));
     }
 }
