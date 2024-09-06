9bdd154ed60008beab5e08b8a9a9a7864aac8cb0
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-09-06 15:59:50 +0530

    Update LambdaA

--- src/com/jbk/lambda/LambdaA.java
+++ src/com/jbk/lambda/LambdaA.java
@@ -1,7 +1,10 @@
 package com.jbk.lambda;
 
 public interface LambdaA {
+    //Single Instance Method one and only method based (Functional Interface).
     int m1 (int a, int b);
+
+    //Cannot Overload the methods or have multiple methods in the functional interface.
     //int m1(int a, int b, int c);
 
 }
