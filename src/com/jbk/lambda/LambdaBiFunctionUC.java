764fc34bdda608fe8ce9dcd79c593e627ec74bfd
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-09-06 19:48:29 +0530

    Update LambdaBiFunctionUC

--- src/com/jbk/lambda/LambdaBiFunctionUC.java
+++ src/com/jbk/lambda/LambdaBiFunctionUC.java
@@ -9,5 +9,13 @@ public class LambdaBiFunctionUC {
 
         obj = (a, b) -> (a + b);
         System.out.println ("Addition using Predefined function.BiFunction Interface: " + obj.apply (10, 20));
+
+        //Trying to create Lambda Expression for a (Void Method without Parameters).
+        Runnable obj1;
+        obj1 = () -> System.out.println ("Hello");
+        obj1.run ( );
+
+        //Also try a functional interface with no parameters but has a return type.
+
     }
 }
