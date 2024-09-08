1038c0270fa07337d34f149c2fa6c57543918e5b
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-09-08 18:22:38 +0530

    Initial ReturnTypeNoParameterInterface

---
+++ src/com/jbk/lambda/ReturnTypeNoParameterInterface.java
@@ -0,0 +1,7 @@
+package com.jbk.lambda;
+
+@FunctionalInterface
+public interface ReturnTypeNoParameterInterface {
+    int m1 ();
+    //Functional Interface having a Single Method with Return Type but with No Parameters.
+}
