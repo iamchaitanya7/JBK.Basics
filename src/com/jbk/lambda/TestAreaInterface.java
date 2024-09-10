b3569cb8dfa8868f45ac9c21419ec07663433f61
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-09-10 15:31:40 +0530

    Update TestAreaInterface

--- src/com/jbk/lambda/TestAreaInterface.java
+++ src/com/jbk/lambda/TestAreaInterface.java
@@ -4,9 +4,12 @@ public class TestAreaInterface {
     public static void main (String[] args) {
         //WAP to calculate the Area of Circle.
         AreaInterface obj;
-        obj = () -> {
+        //Using the inbuilt (Math Class) methods to calculate area.
+        obj = (radius) -> Math.PI * Math.pow (radius, 2);
+        System.out.println ("Area of Circle: " + obj.calculateArea (5));
 
-
-        };
+        //WAP to calculate the Area of Square
+        AreaInterface obj1 = (side) -> Math.pow (side, 2);
+        System.out.println ("Area of Square: " + obj1.calculateArea (10));
     }
 }
