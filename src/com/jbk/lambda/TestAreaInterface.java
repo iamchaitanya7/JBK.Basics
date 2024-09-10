96b69503f96f41f3e5a8605f54cf17fa59cd52be
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-09-10 15:25:11 +0530

    Initial TestAreaInterface

---
+++ src/com/jbk/lambda/TestAreaInterface.java
@@ -0,0 +1,12 @@
+package com.jbk.lambda;
+
+public class TestAreaInterface {
+    public static void main (String[] args) {
+        //WAP to calculate the Area of Circle.
+        AreaInterface obj;
+        obj = () -> {
+
+
+        };
+    }
+}
