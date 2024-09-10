639affa1064cabca6e8fa29e3a4257e0fbf35640
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-09-10 17:43:37 +0530

    Update TestProductReader

--- src/com/jbk/file_handling/TestProductReader.java
+++ src/com/jbk/file_handling/TestProductReader.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class TestProductReader {
     public static void main (String[] args) throws Exception {
 
-        ObjectInputStream objectInputStream = new ObjectInputStream (new FileInputStream (new File ("D:\\A.Workspace's\\JBK\\JBK.Basics\\src\\com\\jbk\\file_handling\\ObjectFile.txt")));
+        ObjectInputStream objectInputStream = new ObjectInputStream (new FileInputStream (new File ("D:\\A.Workspace's\\JBK\\JBK.Basics\\src\\com\\jbk\\file_handling\\targetObjectFile.txt")));
 
         ArrayList<Product> products;
         products = (ArrayList<Product>) objectInputStream.readObject ( );
