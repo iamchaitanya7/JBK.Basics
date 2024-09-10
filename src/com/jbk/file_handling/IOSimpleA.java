47fa879b08e3584c8b566e9fe3522469bfacc5a5
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-09-10 15:50:07 +0530

    Update IOSimpleA

--- src/com/jbk/file_handling/IOSimpleA.java
+++ src/com/jbk/file_handling/IOSimpleA.java
@@ -7,12 +7,14 @@ import java.io.IOException;
 
 public class IOSimpleA {
     public static void main (String[] args) throws IOException {
-        String str = "Hello, World.........!";
+        String str = "Hello, India.........!";
         byte[] x = str.getBytes ( );
 
         File file = new File ("D:\\A.Workspace's\\JBK\\JBK.Basics\\src\\com\\jbk\\file_handling\\targetFile.txt");
 
-        FileOutputStream fileOutputStream = new FileOutputStream (file);
+        //FileOutputStream fileOutputStream = new FileOutputStream (file);
+        FileOutputStream fileOutputStream = new FileOutputStream (file, true);
+
         fileOutputStream.write (x);
         System.out.println ("Data is Written into the file Successfully...!");
         fileOutputStream.close ( );
