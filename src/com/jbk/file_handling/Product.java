be21456ebf523118a143e90e482eeffd0984feb9
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-09-10 17:38:57 +0530

    Initial Product

---
+++ src/com/jbk/file_handling/Product.java
@@ -0,0 +1,52 @@
+package com.jbk.file_handling;
+
+import java.io.Serializable;
+
+public class Product implements Serializable {
+    private long id;
+    private String name;
+    private double price;
+
+    public Product () {
+
+    }
+
+    public Product (long id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public long getId () {
+        return id;
+    }
+
+    public void setId (long id) {
+        this.id = id;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public double getPrice () {
+        return price;
+    }
+
+    public void setPrice (double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString () {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
