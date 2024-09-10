a9010a8e141a0e3ac9715697d84087cd5ee756b4
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-09-10 17:40:28 +0530

    Initial TestProductReader

---
+++ src/com/jbk/file_handling/TestProductReader.java
@@ -0,0 +1,17 @@
+package com.jbk.file_handling;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class TestProductReader {
+    public static void main (String[] args) throws Exception {
+
+        ObjectInputStream objectInputStream = new ObjectInputStream (new FileInputStream (new File ("D:\\A.Workspace's\\JBK\\JBK.Basics\\src\\com\\jbk\\file_handling\\ObjectFile.txt")));
+
+        ArrayList<Product> products;
+        products = (ArrayList<Product>) objectInputStream.readObject ( );
+        objectInputStream.close ( );
+        products.forEach (product -> System.out.println (product));
+
+    }
+}
