78b902567c0bad7cf166ba2413b0f253e5580e61
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-09-10 15:02:37 +0530

    Update TestObjectClassInterface

--- src/com/jbk/lambda/TestObjectClassInterface.java
+++ src/com/jbk/lambda/TestObjectClassInterface.java
@@ -1,4 +1,12 @@
 package com.jbk.lambda;
 
 public class TestObjectClassInterface {
+    public static void main (String[] args) {
+        ObjectClassInterface obj;
+        obj = () -> {
+            for (int i = 0; i <= 10; i++) {
+                System.out.println (i);
+            }
+        };
+    }
 }
