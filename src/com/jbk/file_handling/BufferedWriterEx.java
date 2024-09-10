6412b00d93bace2279e420813cf8e45de108f15a
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-09-10 16:38:33 +0530

    Update BufferedWriterEx

--- src/com/jbk/file_handling/BufferedWriterEx.java
+++ src/com/jbk/file_handling/BufferedWriterEx.java
@@ -1,8 +1,23 @@
 package com.jbk.file_handling;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 public class BufferedWriterEx {
-    public static void main (String[] args) {
-        String[] str = {"Hello", "Chaitanya"};
+    public static void main (String[] args) throws Exception {
+        String[] str = {"Hello", "Chaitanya", "Akash", "Pratik"};
+
+        FileWriter fileWriter = new FileWriter ("D:\\A.Workspace's\\JBK\\JBK.Basics\\src\\com\\jbk\\file_handling\\targetBufferedFile.txt");
+        BufferedWriter bufferedWriter = new BufferedWriter (fileWriter);
+
+        // Write the content to the file
+        bufferedWriter.write (str[0]);
+        //bufferedWriter.newLine ();            Used to append the data onto newLine of the file.
+        bufferedWriter.write (str[1]);
+        bufferedWriter.write (str[2]);
+        bufferedWriter.write (str[3]);
+        bufferedWriter.close ( );
+        System.out.println ("Data written successfully to targetBufferedFile.txt!");
 
     }
 }
