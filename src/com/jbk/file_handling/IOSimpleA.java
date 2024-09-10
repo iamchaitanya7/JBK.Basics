abc649dfc77c4513d55395179c81241f0f41347f
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-09-10 15:42:56 +0530

    Initial IOSimpleA

---
+++ src/com/jbk/file_handling/IOSimpleA.java
@@ -0,0 +1,23 @@
+package com.jbk.file_handling;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class IOSimpleA {
+    public static void main (String[] args) throws IOException {
+        String str = "Hello, World.........!";
+        byte[] x = str.getBytes ( );
+
+        File file = new File ("D:\\A.Workspace's\\JBK\\JBK.Basics\\src\\com\\jbk\\file_handling\\targetFile.txt");
+
+        FileOutputStream fileOutputStream = new FileOutputStream (file);
+        fileOutputStream.write (x);
+        System.out.println ("Data is Written into the file Successfully...!");
+        fileOutputStream.close ( );
+    }
+}
+
+//For Reading the Data -> InputStream.
+//For Writing the Data -> OutputStream.
