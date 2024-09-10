2ceb4d0fe90d80aff0542b579479f1bd7986f458
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-09-10 15:59:18 +0530

    Update IOSimpleWriteA

--- src/com/jbk/file_handling/IOSimpleWriteA.java
+++ src/com/jbk/file_handling/IOSimpleWriteA.java
@@ -1,11 +1,10 @@
 package com.jbk.file_handling;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class IOSimpleA {
+public class IOSimpleWriteA {
     public static void main (String[] args) throws IOException {
         String str = "Hello, India.........!";
         byte[] x = str.getBytes ( );
